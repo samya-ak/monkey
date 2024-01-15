920800a13e581fcc0cc92a7e66110967302cab94
samya-ak <samyakmhn@gmail.com>
2024-01-15 15:20:09 +0545

    lexer: init tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/samya-ak/monkey
+
+go 1.21.6
---
+++ src/lexer/lexer_test.go
@@ -0,0 +1,39 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/samya-ak/monkey/src/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(),;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RPAREN, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NewxToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=-%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
