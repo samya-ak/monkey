9fbdc7efce1e08f8afd04b7699c94bc31aa50599
samya-ak <samyakmhn@gmail.com>
2024-07-08 22:38:29 +0545

    init ast and parser

---
+++ src/parser/parser.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"github.com/samya-ak/monkey/src/ast"
+	"github.com/samya-ak/monkey/src/lexer"
+	"github.com/samya-ak/monkey/src/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
